16d12ff65ab364292cb4b571b425d6a64d9d4709
Peter Cavanagh <peter.cavanagh2@nhs.net>
2022-06-23 11:06:39 +0100

    fixed duplicate account

--- terraform/dummy_accounts.tf
+++ terraform/dummy_accounts.tf
@@ -84,34 +84,6 @@ module "dummy_02" {
 }
 
 
-module "dummy_02" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "peter.cavanagh2+dummy-02@nhs.net"
-    AccountName               = "dummy-02"
-    ManagedOrganizationalUnit = "Sandpit"
-    SSOUserEmail              = "peter.cavanagh2+dummy-02@nhs.net"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "dummy-02"
-}
-
 module "dummy_03" {
   source = "./modules/aft-account-request"
 
