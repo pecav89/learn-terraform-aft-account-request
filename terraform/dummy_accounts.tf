b9e0900b94791d3ca7d50228492e62dab2ebd0af
Peter Cavanagh <peter.cavanagh2@nhs.net>
2022-06-23 09:35:43 +0100

    added a failure account

---
+++ terraform/dummy_accounts.tf
@@ -0,0 +1,27 @@
+module "dummy_failure" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "peter.cavanagh2+dummy-failure@nhs.net"
+    AccountName               = "Failure Account"
+    ManagedOrganizationalUnit = "Sandpit"
+    SSOUserEmail              = "peter.cavanagh2+dummy-failure@nhs.net"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "Failure Account"
+}
