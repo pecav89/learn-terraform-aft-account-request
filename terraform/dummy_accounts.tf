57f45cb1e708a9ef7901472796978d16f15a24b5
Peter Cavanagh <peter.cavanagh2@nhs.net>
2022-07-04 13:34:45 +0100

    removed dummy-04 account

--- terraform/dummy_accounts.tf
+++ terraform/dummy_accounts.tf
@@ -26,33 +26,3 @@ module "dummy_03" {
 
   account_customizations_name = "dummy-03"
 }
-
-
-module "dummy_04" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "peter.cavanagh2+dummy-04@nhs.net"
-    AccountName               = "dummy-04"
-    ManagedOrganizationalUnit = "Sandpit"
-    SSOUserEmail              = "peter.cavanagh2+dummy-04@nhs.net"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-    "Dummy Account"  = true
-  }
-
-  change_management_parameters = {
-    change_requested_by = "Peter Cavanagh"
-    change_reason       = "Add dummy account"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "dummy-04"
-}
